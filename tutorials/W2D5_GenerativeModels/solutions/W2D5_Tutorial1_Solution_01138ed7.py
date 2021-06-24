5d3c0cd2d577b5bf310fffd815fb0832512d4457
Spiros Ch <spirosChv@users.noreply.github.com>
2021-06-25 00:08:13 +0300

    W2d5 reformatting (#53)
    * Update and rename W2D5_Tutorial4.ipynb to W2D5_Tutorial5.ipynb
    
    * Delete W2D5_Tutorial1.ipynb
    
    * Delete W2D5_Tutorial2.ipynb
    
    * Delete W2D5_Tutorial3.ipynb
    
    * Delete W2D5_Tutorial4.ipynb
    
    * Delete .gitkeep
    
    * Delete W2D5_Tutorial1_Solution_dc5b38a9.py
    
    * Delete W2D5_Tutorial2_Solution_007bde00.py
    
    * Delete W2D5_Tutorial2_Solution_04a08f8f.py
    
    * Delete W2D5_Tutorial2_Solution_393f89f3.py
    
    * Delete W2D5_Tutorial2_Solution_007bde00_1.png
    
    * Delete W2D5_Tutorial2_Solution_04a08f8f_1.png
    
    * Delete W2D5_Tutorial2_Solution_393f89f3_1.png
    
    * t1 t2 merged in new t1
    
    * Delete W2D5_Tutorial1.ipynb
    
    * Delete W2D5_Tutorial2.ipynb
    
    * Delete W2D5_Tutorial3.ipynb
    
    * Delete W2D5_Tutorial5.ipynb
    
    * vae tutorial
    
    * merged old t1 and t2
    
    * merged old t3 and t4
    
    * Delete README.md
    
    * fixing code with close mismatch in ex 1, and 2
    
    * Process tutorial notebooks
    
    * fixing seed in DataLoader t1
    
    * ci checks
    
    * fix ci t3
    
    * Process tutorial notebooks
    
    * Update W2D5_Tutorial3.ipynb
    
    * re run to pass ci
    
    * re run to pass ci
    
    * minor fixes in imports
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>

---
+++ tutorials/W2D5_GenerativeModels/solutions/W2D5_Tutorial1_Solution_01138ed7.py
@@ -0,0 +1,22 @@
+def generate_images(autoencoder, K, n_images=1):
+  """Generate n_images 'new' images from the decoder part of the given
+  autoencoder.
+
+  returns (n_images, channels, height, width) tensor of images
+  """
+  # Concatenate tuples to get (n_images, channels, height, width)
+  output_shape = (n_images,) + my_dataset_size
+  with torch.no_grad():
+    # sample z, pass through autoencoder.decode(), and reshape output.
+    z = torch.randn(n_images, K)
+    x = autoencoder.decode(z).reshape(output_shape)
+
+    return x
+
+
+K = 20
+set_seed(2021)
+# Uncomment to run it
+images = generate_images(conv_ae, K, n_images=25)
+with plt.xkcd():
+  plot_images(images)
---
+++ tutorials/W2D5_GenerativeModels/solutions/W2D5_Tutorial1_Solution_2c25dcf6.py
@@ -0,0 +1,22 @@
+def rsample(phi, n_samples):
+  """Sample z ~ q(z;phi)
+  Ouput z is size [b,n_samples,K] given phi with shape [b,K+1]. The first K
+  entries of each row of phi are the mean of q, and phi[:,-1] is the log
+  standard deviation
+  """
+  b, kplus1 = phi.size()
+  k = kplus1 - 1
+  mu, sig = phi[:, :-1], phi[:, -1].exp()
+  eps = torch.randn(b, n_samples, k, device=phi.device)
+  return eps*sig.view(b, 1, 1) + mu.view(b, 1, k)
+
+
+phi = torch.randn(4, 3, device=DEVICE)
+set_seed(2021)
+# Uncomment below to test your code
+zs = rsample(phi, 100)
+assert zs.size() == (4, 100, 2), "rsample size is incorrect!"
+assert zs.device == phi.device, "rsample device doesn't match phi device!"
+zs = zs.cpu()
+with plt.xkcd():
+  plot_phi(phi)
