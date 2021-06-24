f44378228870f4c757fea51b455f062b6d86f255
GitHub Action <action@github.com>
2021-06-24 20:02:12 +0000

    Process tutorial notebooks

---
+++ tutorials/W2D5_GenerativeModels/solutions/W2D5_Tutorial2_Solution_04a08f8f.py
@@ -0,0 +1,26 @@
+def loss_gen(disc, x_fake):
+  """Compute the generator loss for `x_fake` given `disc`
+
+  Args:
+    disc: The generator
+    x_fake (ndarray): An array of shape (N,) that contains the fake samples
+
+  Returns:
+    ndarray: The generator loss
+  """
+
+  # Loss for fake data
+  label_fake = 1
+  loss_fake = label_fake * torch.log(disc.classify(x_fake))
+
+  return loss_fake
+
+
+disc = DummyDisc()
+gen = DummyGen()
+
+x_fake = gen.sample()
+## Uncomment below to check your function
+lg = loss_gen(disc, x_fake)
+with plt.xkcd():
+  plotting_lg(lg)
