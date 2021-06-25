aa3daac28dbdeadf725ff350b575a983f1310ed4
GitHub Action <action@github.com>
2021-06-25 01:21:14 +0000

    Process tutorial notebooks

---
+++ tutorials/W1D1_BasicsAndPytorch/solutions/W1D1_Tutorial1_Solution_82257c1c.py
@@ -0,0 +1,6 @@
+
+# TODO: Uncomment the following line to see the error that arises from the current image format
+# plt.imshow(image)
+
+# TODO: Comment the above line and fix this code by reordering the tensor dimensions
+plt.imshow(image.permute(1,2,0))
