09467a679bd1273be114b45a0735d73ac87d803b
Spiros Ch <spirosChv@users.noreply.github.com>
2021-07-02 16:36:05 +0300

    update notebook t2 (#83)
    * update headers, video cells, add sponsors, seed, device functions, fixes in codestyle
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>

--- tutorials/W1D5_Regularization/solutions/W1D5_Tutorial2_Solution_984c4088.py
+++ tutorials/W1D5_Regularization/solutions/W1D5_Tutorial2_Solution_984c4088.py
@@ -6,13 +6,13 @@ def l2_reg(model):
   """
 
   l2 = 0.0
-
   for param in model.parameters():
     l2 += torch.sum(torch.abs(param)**2)
 
   return l2
 
 
-# # uncomment to test
+set_seed(SEED)
+## uncomment to test
 net = nn.Linear(20, 20)
 print(f'L2 norm of the model: {l2_reg(net)}')
