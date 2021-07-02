1385e0bc772a25fb7ff0977d2c22ce6dd8bc643f
GitHub Action <action@github.com>
2021-07-02 12:59:12 +0000

    Process tutorial notebooks

--- tutorials/W1D5_Regularization/solutions/W1D5_Tutorial1_Solution_bcc74b44.py
+++ tutorials/W1D5_Regularization/solutions/W1D5_Tutorial1_Solution_bcc74b44.py
@@ -1,15 +1,17 @@
 def calculate_frobenius_norm(model):
-
   norm = 0.0
-
   # Sum the square of all parameters
   for param in model.parameters():
-      norm += torch.sum(param**2)
+    norm += torch.sum(param**2)
 
   # Take a square root of the sum of squares of all the parameters
   norm = norm**0.5
   return norm
 
-# # uncomment to run
+
+# Seed added for reproducibility
+set_seed(seed=SEED)
+
+## uncomment below to test your code
 net = nn.Linear(10, 1)
 print(f'Frobenius Norm of Single Linear Layer: {calculate_frobenius_norm(net)}')
