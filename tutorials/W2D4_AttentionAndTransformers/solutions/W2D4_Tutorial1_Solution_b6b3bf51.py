3c2f358c661d5f56c3b54c6c8f3235d032b9d654
GitHub Action <action@github.com>
2021-07-11 18:56:39 +0000

    Process tutorial notebooks

--- tutorials/W2D4_AttentionAndTransformers/solutions/W2D4_Tutorial1_Solution_b6b3bf51.py
+++ tutorials/W2D4_AttentionAndTransformers/solutions/W2D4_Tutorial1_Solution_b6b3bf51.py
@@ -4,13 +4,15 @@ class DotProductAttention(nn.Module):
     super(DotProductAttention, self).__init__(**kwargs)
     self.dropout = nn.Dropout(dropout)
 
-  def forward(self, queries, keys, values, b, h, t, k):
+  def forward(self, queries, keys, values, b, h, t, d):
     """
     Compute dot products. This is the same operation for each head,
     so we can fold the heads into the batch dimension and use torch.bmm
     Note: .contiguous() doesn't change the actual shape of the data,
     but it rearranges the tensor in memory, which will help speed up the computation
     for this batch matrix multiplication.
+    .transpose(dim0, dim1) is used to change the shape of a tensor. It returns a new tensor
+    that shares the data with the original tensor. It can only swap two dimension.
 
     Shape of `queries`: (`batch_size`, no. of queries, `d`)
     Shape of `keys`: (`batch_size`, no. of key-value pairs, `d`)
@@ -19,18 +21,18 @@ class DotProductAttention(nn.Module):
     b: batch size
     h: number of heads
     t: number of keys/queries/values (for simplicity, let's assume they have the same sizes)
-    k: embedding size
+    d: embedding size
     """
-    keys = keys.transpose(1, 2).contiguous().view(b * h, t, k)
-    queries = queries.transpose(1, 2).contiguous().view(b * h, t, k)
-    values = values.transpose(1, 2).contiguous().view(b * h, t, k)
+    keys = keys.transpose(1, 2).contiguous().view(b * h, t, d)
+    queries = queries.transpose(1, 2).contiguous().view(b * h, t, d)
+    values = values.transpose(1, 2).contiguous().view(b * h, t, d)
 
     # Matrix Multiplication between the keys and queries
-    score = torch.bmm(queries, keys.transpose(1, 2)) / math.sqrt(k)  # size: (b * h, t, t)
+    score = torch.bmm(queries, keys.transpose(1, 2)) / math.sqrt(d)  # size: (b * h, t, t)
     softmax_weights = F.softmax(score, dim=2)  # row-wise normalization of weights
 
     # Matrix Multiplication between the output of the key and queries multiplication and values.
-    out = torch.bmm(self.dropout(softmax_weights), values).view(b, h, t, k)  # rearrange h and t dims
-    out = out.transpose(1, 2).contiguous().view(b, t, h * k)
+    out = torch.bmm(self.dropout(softmax_weights), values).view(b, h, t, d)  # rearrange h and t dims
+    out = out.transpose(1, 2).contiguous().view(b, t, h * d)
 
     return out
