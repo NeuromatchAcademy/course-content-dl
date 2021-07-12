938b938ce5ee7248222596c9da5ad66b20776b9e
GitHub Action <action@github.com>
2021-07-12 14:20:45 +0000

    Process tutorial notebooks

---
+++ tutorials/W1D1_BasicsAndPytorch/solutions/W1D1_Tutorial1_Solution_2a69ad55.py
@@ -0,0 +1,12 @@
+def dot_product(b1: torch.Tensor, b2: torch.Tensor):
+  # Use torch.dot() to compute the dot product of two tensors
+  product = torch.dot(b1, b2)
+  return product
+
+
+# Computing expression 2:
+b1 = torch.tensor([3, 5, 7])
+b2 = torch.tensor([2, 4, 8])
+## Uncomment to test your function
+b = dot_product(b1, b2)
+print(b)
