6dafa538286d9292177f1aefa50504b8634e39c5
GitHub Action <action@github.com>
2021-07-15 08:49:39 +0000

    Process tutorial notebooks

---
+++ tutorials/W2D2_ModernConvnets/solutions/W2D2_Tutorial1_Solution_b92b2f40.py
@@ -0,0 +1,13 @@
+
+"""
+1. Algorithms will have problems to distinguish people of this group
+   if the distances of the embeddings are smaller.
+
+2. Many examples possible
+  - Unblocking the smartphone with a face recognition system might not work or not be secure.
+  - Surveillance cameras might confuse indivduals.
+  - Social network automated tagging might confuse individuals.
+
+3. Train the model on a balanced dataset with enough samples for each minority.
+   If you use pre-trained models, obtain information about the datasets they were pretrained on.
+"""
