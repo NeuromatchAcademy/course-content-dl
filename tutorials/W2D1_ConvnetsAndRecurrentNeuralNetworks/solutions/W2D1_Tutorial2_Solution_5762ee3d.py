1c1a328f865f84318280c364445f93ad497eda8d
GitHub Action <action@github.com>
2021-07-18 21:22:34 +0000

    Process tutorial notebooks

--- tutorials/W2D1_ConvnetsAndRecurrentNeuralNetworks/solutions/W2D1_Tutorial2_Solution_5762ee3d.py
+++ tutorials/W2D1_ConvnetsAndRecurrentNeuralNetworks/solutions/W2D1_Tutorial2_Solution_5762ee3d.py
@@ -8,7 +8,7 @@ set_seed(SEED)
 net3 = FMNIST_Net2().to(DEVICE)  # get the network
 
 ## Uncomment below to test your function
-train_loader, validation_loader, test_loader = tranfroms_custom()
+train_loader, validation_loader, test_loader = transforms_custom()
 train_loss, train_acc, validation_loss, validation_acc = train(net3, DEVICE, train_loader, validation_loader, 20)
 with plt.xkcd():
   plot_loss_accuracy(train_loss, train_acc, validation_loss, validation_acc)
