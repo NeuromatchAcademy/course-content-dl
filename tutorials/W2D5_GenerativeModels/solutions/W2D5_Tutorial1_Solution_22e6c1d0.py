9ce5837c391b0a32c2b3ede58dd803ecbb0a91fd
Saeed Salehi <49148519+ssnio@users.noreply.github.com>
2021-07-20 23:59:04 +0200

    W2 d5 t1 feedbacks (#154)
    * Fix Prepod feedbacks part 1
    
    * Fix Prepod feedbacks part 2
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>

--- tutorials/W2D5_GenerativeModels/solutions/W2D5_Tutorial1_Solution_22e6c1d0.py
+++ tutorials/W2D5_GenerativeModels/solutions/W2D5_Tutorial1_Solution_22e6c1d0.py
@@ -8,12 +8,12 @@ def rsample(phi, n_samples):
   k = kplus1 - 1
   mu, sig = phi[:, :-1], phi[:, -1].exp()
   eps = torch.randn(b, n_samples, k, device=phi.device)
-  return eps*sig.view(b, 1, 1) + mu.view(b, 1, k)
+  return eps * sig.view(b, 1, 1) + mu.view(b, 1, k)
 
 
+# # Uncomment below to test your code
 phi = torch.randn(4, 3, device=DEVICE)
 set_seed(2021)
-# Uncomment below to test your code
 zs = rsample(phi, 100)
 assert zs.size() == (4, 100, 2), "rsample size is incorrect!"
 assert zs.device == phi.device, "rsample device doesn't match phi device!"
