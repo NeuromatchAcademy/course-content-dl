437e8d2ddb1a924aa0faad218471339621f5b7c6
Spiros Ch <spirosChv@users.noreply.github.com>
2021-07-23 05:27:05 +0300

    ci:execute fixes (#187)
    * ci:execute fixes
    
    * update
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>

--- tutorials/W2D5_GenerativeModels/solutions/W2D5_Tutorial1_Solution_a4b2811e.py
+++ tutorials/W2D5_GenerativeModels/solutions/W2D5_Tutorial1_Solution_a4b2811e.py
@@ -1,9 +1,11 @@
-def rsample(phi, n_samples):
+def rsample(phi, n_samples, seed=0):
   """Sample z ~ q(z;phi)
   Ouput z is size [b,n_samples,K] given phi with shape [b,K+1]. The first K
   entries of each row of phi are the mean of q, and phi[:,-1] is the log
   standard deviation
   """
+
+  set_seed(seed=seed)
   b, kplus1 = phi.size()
   k = kplus1 - 1
   mu, sig = phi[:, :-1], phi[:, -1].exp()
@@ -12,9 +14,8 @@ def rsample(phi, n_samples):
 
 
 phi = torch.randn(4, 3, device=DEVICE)
-set_seed(seed=SEED)
 ## Uncomment below to test your code
-zs = rsample(phi, 100)
+zs = rsample(phi=phi, n_samples=100, seed=SEED)
 assert zs.size() == (4, 100, 2), "rsample size is incorrect!"
 assert zs.device == phi.device, "rsample device doesn't match phi device!"
 zs = zs.cpu()
