7c1dfc7f6010ba323ecf88953b2ed882e1551b87
Spiros Ch <spirosChv@users.noreply.github.com>
2021-07-25 04:31:52 +0300

    Add bilibili (#221)
    * add bilibili links
    
    * add bilibili t2
    
    * add bilibili w1d2 t3
    
    * bilibili links on w1d3 t1
    
    * bilinili and ethics video in w1d3 t2
    
    * add bilibili and one missing video w2d3 t1
    
    * add bilibili w2d3 t2
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>

--- tutorials/W1D3_MultiLayerPerceptrons/solutions/W1D3_Tutorial1_Solution_40ab8faf.py
+++
@@ -1,6 +0,0 @@
-
-"""
-At infinite membrane resistance, the Neuron does not leak any current out, and hence it starts firing with the slightest input current,
-This shifts the transfer function towards 0, similar to ReLU activation (centered at 0).
-Also, when there is minimal refractory time, the neuron can keep firing at a high input current which avoids the saturation.
-""";
