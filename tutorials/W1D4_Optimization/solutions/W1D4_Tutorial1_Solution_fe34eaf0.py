77fe39e5efc9b75730234b0e4ff91074cff0cf19
GitHub Action <action@github.com>
2021-07-27 13:59:31 +0000

    Process tutorial notebooks

--- tutorials/W1D4_Optimization/solutions/W1D4_Tutorial1_Solution_fe34eaf0.py
+++ tutorials/W1D4_Optimization/solutions/W1D4_Tutorial1_Solution_fe34eaf0.py
@@ -1,32 +1,35 @@
-def gradient_update(loss, params, lr=1e-3):
-  """Perform a gradient descent update on a given loss over a collection of parameters
+def momentum_update(loss, params, grad_vel, lr=1e-3, beta=0.8):
+  """Perform a momentum update over a collection of parameters given a loss and 'velocities'
 
   Args:
     loss (tensor): A scalar tensor containing the loss whose gradient will be computed
     params (iterable): Collection of parameters with respect to which we compute gradients
+    grad_vel (iterable): Collection containing the 'velocity' v_t for each parameter
     lr (float): Scalar specifying the learning rate or step-size for the update
+    beta (float): Scalar 'momentum' parameter
   """
   # Clear up gradients as Pytorch automatically accumulates gradients from
   # successive backward calls
   zero_grad(params)
-
   # Compute gradients on given objective
   loss.backward()
 
   with torch.no_grad():
-    for par in params:
-      # Here we work with the 'data' attribute of the parameter rather than the
-      # parameter itself.
-      par -= lr * par.grad
+    for (par, vel) in zip(params, grad_vel):
+      # Update 'velocity'
+      vel = -lr * par.grad + beta * vel
+      # Update parameters
+      par += vel
 
 
 set_seed(2021)
-model = MLP(in_dim=784, out_dim=10, hidden_dims=[]).to(DEVICE)
+model = MLP(in_dim=784, out_dim=10, hidden_dims=[])
 print('\n The model parameters before the update are: \n')
 print_params(model)
-loss = loss_fn(model(X), y).to(DEVICE)
+loss = loss_fn(model(X), y)
+initial_vel = [torch.randn_like(p) for p in model.parameters()]
 
 ## Uncomment below to test your function
-gradient_update(loss, list(model.parameters()), lr=1e-1)
+momentum_update(loss, list(model.parameters()), grad_vel=initial_vel, lr=1e-1)
 print('\n The model parameters after the update are: \n')
 print_params(model)
