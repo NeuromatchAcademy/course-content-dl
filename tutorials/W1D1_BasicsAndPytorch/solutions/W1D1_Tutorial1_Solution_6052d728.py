377609c50b482ba46b1604186f8c7944b56e1447
GitHub Action <action@github.com>
2021-07-28 13:39:12 +0000

    Process tutorial notebooks

--- tutorials/W1D1_BasicsAndPytorch/solutions/W1D1_Tutorial1_Solution_6052d728.py
+++ tutorials/W1D1_BasicsAndPytorch/solutions/W1D1_Tutorial1_Solution_6052d728.py
@@ -6,8 +6,11 @@ def my_data_load():
   # Display a random grayscale image
   image, label = data[random.randint(0, len(data))]
   plt.imshow(image.squeeze(), cmap="gray")
+  plt.show()
+
+  return data
 
 
 set_seed(seed=2021)
 ## After implementing the above code, uncomment the following lines to test your code
-my_data_load()
+data = my_data_load()
