08cca7da6a9a7e8c5a9fff7587a9ccc1cbbfff9e
GitHub Action <action@github.com>
2021-07-28 11:52:59 +0000

    Process tutorial notebooks

---
+++ tutorials/W1D1_BasicsAndPytorch/solutions/W1D1_Tutorial1_Solution_bc54300f.py
@@ -0,0 +1,79 @@
+def functionA(my_tensor1, my_tensor2):
+  """
+  This function takes in two 2D tensors `my_tensor1` and `my_tensor2`
+  and returns the column sum of
+  `my_tensor1` multiplied by the sum of all the elmements of `my_tensor2`,
+  i.e., a scalar.
+
+  Args:
+    my_tensor1: torch.Tensor
+    my_tensor2: torch.Tensor
+  Retuns:
+    output: torch.Tensor
+      The multiplication of the column sum of `my_tensor1` by the sum of
+      `my_tensor2`.
+  """
+  # TODO multiplication the sum of the tensors
+  output = my_tensor1.sum(axis=0) * my_tensor2.sum()
+
+  return output
+
+
+def functionB(my_tensor):
+  """
+  This function takes in a square matrix `my_tensor` and returns a 2D tensor
+  consisting of a flattened `my_tensor` with the index of each element
+  appended to this tensor in the row dimension.
+
+  Args:
+    my_tensor: torch.Tensor
+  Retuns:
+    output: torch.Tensor
+      Concatenated tensor.
+  """
+  # TODO flatten the tensor `my_tensor`
+  my_tensor = my_tensor.flatten()
+  # TODO create the idx tensor to be concatenated to `my_tensor`
+  idx_tensor = torch.arange(0, len(my_tensor))
+  # TODO concatenate the two tensors
+  output = torch.cat([idx_tensor.unsqueeze(1), my_tensor.unsqueeze(1)], axis=1)
+
+  return output
+
+
+def functionC(my_tensor1, my_tensor2):
+  """
+  This function takes in two 2D tensors `my_tensor1` and `my_tensor2`.
+  If the dimensions allow it, it returns the
+  elementwise sum of `my_tensor1`-shaped `my_tensor2`, and `my_tensor2`;
+  else this function returns a 1D tensor that is the concatenation of the
+  two tensors.
+
+  Args:
+    my_tensor1: torch.Tensor
+    my_tensor2: torch.Tensor
+  Retuns:
+    output: torch.Tensor
+      Concatenated tensor.
+  """
+  # TODO check we can reshape `my_tensor2` into the shape of `my_tensor1`
+  if torch.numel(my_tensor1) == torch.numel(my_tensor2):
+    # TODO reshape `my_tensor2` into the shape of `my_tensor1`
+    my_tensor2 = my_tensor2.reshape(my_tensor1.shape)
+    # TODO sum the two tensors
+    output = my_tensor1 + my_tensor2
+  else:
+    # TODO flatten both tensors
+    my_tensor1 = my_tensor1.reshape(1, -1)
+    my_tensor2 = my_tensor2.reshape(1, -1)
+    # TODO concatenate the two tensors in the correct dimension
+    output = torch.cat([my_tensor1, my_tensor2], axis=1)
+
+  return output
+
+
+## Implement the functions above and then uncomment the following lines to test your code
+print(functionA(torch.tensor([[1, 1], [1, 1]]), torch.tensor([[1, 2, 3], [1, 2, 3]])))
+print(functionB(torch.tensor([[2, 3], [-1, 10]])))
+print(functionC(torch.tensor([[1, -1], [-1, 3]]), torch.tensor([[2, 3, 0, 2]])))
+print(functionC(torch.tensor([[1, -1], [-1, 3]]), torch.tensor([[2, 3, 0]])))
