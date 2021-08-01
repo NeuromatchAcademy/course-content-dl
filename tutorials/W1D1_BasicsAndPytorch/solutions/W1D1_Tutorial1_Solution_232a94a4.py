31754e537615087282d084c0a2009f84a7cefe18
Spiros Ch <spirosChv@users.noreply.github.com>
2021-08-01 23:19:03 +0300

    W1 d1 submission2 (#339)
    * initial
    
    * minor fix add airtable faq
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>

--- tutorials/W1D1_BasicsAndPytorch/solutions/W1D1_Tutorial1_Solution_232a94a4.py
+++ tutorials/W1D1_BasicsAndPytorch/solutions/W1D1_Tutorial1_Solution_232a94a4.py
@@ -6,17 +6,23 @@ def simpleFun(dim, device):
   Returns:
     Nothing.
   """
+  # 2D tensor filled with uniform random numbers in [0,1), dim x dim
   x = torch.rand(dim, dim).to(device)
+  # 2D tensor filled with uniform random numbers in [0,1), dim x dim
   y = torch.rand_like(x).to(device)
+  # 2D tensor filled with the scalar value 2, dim x dim
   z = 2*torch.ones(dim, dim).to(device)
 
-  x = x * y
-  x = x @ z
-
+  # elementwise multiplication of x and y
+  a = x * y
+  # matrix multiplication of x and y
+  b = x @ z
 
   del x
   del y
   del z
+  del a
+  del b
 
 
 ## TODO: Implement the function above and uncomment the following lines to test your code
