54551d8dac5a73cf6bc2f739b857bbb3069e9b60
Spiros Ch <spirosChv@users.noreply.github.com>
2021-08-10 00:40:57 +0300

    minor fixes (#456)
    * minor fixes
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>

--- tutorials/W2D2_ModernConvnets/solutions/W2D2_Tutorial2_Solution_866d0881.py
+++
@@ -1,4 +0,0 @@
-
-"""
-The distances between black female embeddings are generally lower than the distances between white female embeddings, i.e. for the Face Detection algorithm faces of black females are more similar to each other.
-"""
