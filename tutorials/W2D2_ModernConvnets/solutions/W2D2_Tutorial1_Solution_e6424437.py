7efbe381444ca044790c4088873eab901ea35af1
Spiros Ch <spirosChv@users.noreply.github.com>
2021-08-19 15:28:59 +0300

    W3d3 typo (#512)
    * fix typo
    
    * fix typo
    
    * skip ci
    
    * restart and run
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>

--- tutorials/W2D2_ModernConvnets/solutions/W2D2_Tutorial1_Solution_e6424437.py
+++ tutorials/W2D2_ModernConvnets/solutions/W2D2_Tutorial1_Solution_e6424437.py
@@ -3,9 +3,9 @@
 1. The closer your pretraining and target data domains are, the better pretraining will work
 2. The more pretraining data you have, the better pretraining will work
 3. The better your model is able to take advantage of your pretraining data (that is to say
-   the larger your model is asResNetsuming you have enough data), the better pretraing will work
+   the larger your model is as you have enough data), the better pretraining will work
 
-Pretraining isn't necessarily always a benefit though. If you source domain is very different from
+Pretraining isn't necessarily always a benefit though. If your source domain is very different from
 the domain you're trying to predict, your models might learn unhelpful features.
 
 Additionally, if you have a lot of training data in your target domain, pretraining data might
