e81a5ece2bec9b0a965751ad744a49d4a318605a
CodeWizard <GaganaB@users.noreply.github.com>
2021-08-20 11:25:06 +0530

    W3d4 fixes (#516)
    * Created using Colaboratory
    
    * Delete  course-content-dl/tutorials/W3D4_ContinualLearning directory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * fix ex 4.2
    
    * Process tutorial notebooks
    
    Co-authored-by: Spiros Ch <spirosChv@users.noreply.github.com>
    Co-authored-by: GitHub Action <action@github.com>

--- tutorials/W3D4_ContinualLearning/solutions/W3D4_Tutorial1_Solution_77a92249.py
+++
@@ -1,6 +0,0 @@
-"""
-Create a TxT matrix with values between 0 and 1 to
-be used to compute the metrics.
-"""
-T = 5  # number of tasks
-result_matrix = np.random.uniform(low=0, high=1, size=(T, T))  # here put a TxT matrix with values in [0, 1]
