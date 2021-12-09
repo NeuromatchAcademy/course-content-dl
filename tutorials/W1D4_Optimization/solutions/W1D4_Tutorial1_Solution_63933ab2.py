0111e81f123d63937a8706b599b574d5446835cd
Spiros Ch <spirosChv@users.noreply.github.com>
2021-12-09 15:46:11 +0200

    post fixes (#528)
    * post fixes
    
    * Process tutorial notebooks
    
    * fix interactive demo 6.1 to remove the error showed on book
    
    * Process tutorial notebooks
    
    * Created using Colaboratory
    
    * Delete W1D4_Tutorial1.ipynb
    
    * Created using Colaboratory
    
    * Process tutorial notebooks
    
    * minor corrections
    
    * Process tutorial notebooks
    
    Co-authored-by: GitHub Action <action@github.com>
    Co-authored-by: CodeWizard <GaganaB@users.noreply.github.com>

--- tutorials/W1D4_Optimization/solutions/W1D4_Tutorial1_Solution_63933ab2.py
+++ tutorials/W1D4_Optimization/solutions/W1D4_Tutorial1_Solution_63933ab2.py
@@ -1,10 +1,17 @@
 def gradient_update(loss, params, lr=1e-3):
-  """Perform a gradient descent update on a given loss over a collection of parameters
+  """
+  Perform a gradient descent update on a given loss over a collection of parameters
 
   Args:
-    loss (tensor): A scalar tensor containing the loss whose gradient will be computed
-    params (iterable): Collection of parameters with respect to which we compute gradients
-    lr (float): Scalar specifying the learning rate or step-size for the update
+    loss: Tensor
+      A scalar tensor containing the loss through which the gradient will be computed
+    params: List of iterables
+      Collection of parameters with respect to which we compute gradients
+    lr: Float
+      Scalar specifying the learning rate or step-size for the update
+
+  Returns:
+    Nothing
   """
   # Clear up gradients as Pytorch automatically accumulates gradients from
   # successive backward calls
@@ -15,8 +22,13 @@ def gradient_update(loss, params, lr=1e-3):
 
   with torch.no_grad():
     for par in params:
+      #################################################
+      ## TODO for students: update the value of the parameter ##
+      # raise NotImplementedError("Student exercise: implement gradient update")
+      #################################################
       # Here we work with the 'data' attribute of the parameter rather than the
       # parameter itself.
+      # Hence - use the learning rate and the parameter's .grad.data attribute to perform an update
       par.data -= lr * par.grad.data
 
 
--- tutorials/W1D4_Optimization/solutions/W1D4_Tutorial1_Solution_9d60a8af.py
+++ tutorials/W1D4_Optimization/solutions/W1D4_Tutorial1_Solution_9d60a8af.py
@@ -1,13 +1,23 @@
 def rmsprop_update(loss, params, grad_sq, lr=1e-3, alpha=0.8, epsilon=1e-8):
-  """Perform an RMSprop update on a collection of parameters
+  """
+  Perform an RMSprop update on a collection of parameters
 
   Args:
-    loss (tensor): A scalar tensor containing the loss whose gradient will be computed
-    params (iterable): Collection of parameters with respect to which we compute gradients
-    grad_sq (iterable): Moving average of squared gradients
-    lr (float): Scalar specifying the learning rate or step-size for the update
-    alpha (float): Moving average parameter
-    epsilon (float): for numerical estability
+    loss: Tensor
+      A scalar tensor containing the loss whose gradient will be computed
+    params: Iterable
+      Collection of parameters with respect to which we compute gradients
+    grad_sq: Iterable
+      Moving average of squared gradients
+    lr: Float
+      Scalar specifying the learning rate or step-size for the update
+    alpha: Float
+      Moving average parameter
+    epsilon: Float
+      quotient for numerical stability
+
+  Returns:
+    Nothing
   """
   # Clear up gradients as Pytorch automatically accumulates gradients from
   # successive backward calls
@@ -32,7 +42,7 @@ model3 = MLP(in_dim=784, out_dim=10, hidden_dims=[])
 print('\n The model3 parameters before the update are: \n')
 print_params(model3)
 loss = loss_fn(model3(X), y)
-# Intialize the moving average of squared gradients
+# Initialize the moving average of squared gradients
 grad_sq = [1e-6*i for i in list(model3.parameters())]
 
 ## Uncomment below to test your function
