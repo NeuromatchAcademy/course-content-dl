ce9c9dc089104e37c6a06bf856e67369dc48cb0d
GitHub Action <action@github.com>
2021-12-24 11:35:20 +0000

    Process tutorial notebooks

--- tutorials/W1D3_MultiLayerPerceptrons/solutions/W1D3_Tutorial2_Solution_5c80bef1.py
+++ tutorials/W1D3_MultiLayerPerceptrons/solutions/W1D3_Tutorial2_Solution_5c80bef1.py
@@ -35,6 +35,7 @@ def get_data_loaders(batch_size, seed):
 
   return img_train_loader, img_test_loader
 
+
 # add event to airtable
 atform.add_event('Coding Exercise 2: Dataloader on a real-world dataset')
 
