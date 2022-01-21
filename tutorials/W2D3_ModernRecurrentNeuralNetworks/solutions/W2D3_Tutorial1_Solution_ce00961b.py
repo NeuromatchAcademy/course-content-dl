86c06a7107e0a04259ff307a85db326ba7a10db6
GitHub Action <action@github.com>
2022-01-21 12:57:30 +0000

    Process tutorial notebooks

--- tutorials/W2D3_ModernRecurrentNeuralNetworks/solutions/W2D3_Tutorial1_Solution_ce00961b.py
+++ tutorials/W2D3_ModernRecurrentNeuralNetworks/solutions/W2D3_Tutorial1_Solution_ce00961b.py
@@ -1,6 +1,32 @@
 class NeuralNet(nn.Module):
+  """
+  Neural Network with following structure:
+  nn.Embedding(vocab_size, embedding_length)
+  + nn.Parameter(word_embeddings, requires_grad=False) # Embedding Layer
+  nn.Linear(embedding_length, hidden_size) # Fully connected layer #1
+  nn.Linear(hidden_size, output_size) # Fully connected layer #2
+  """
+
   def __init__(self, output_size, hidden_size, vocab_size, embedding_length,
                word_embeddings):
+    """
+    Initialize parameters of NeuralNet
+
+    Args:
+      output_size: int
+        Size of final fully connected layer
+      hidden_size: int
+        Size of hidden/first fully connected layer
+      vocab_size: int
+        Size of vocabulary
+      embedding_length: int
+        Length of embedding
+      word_embeddings: TEXT.vocab.vectors instance
+        Word Embeddings
+
+    Returns:
+      Nothing
+    """
     super(NeuralNet, self).__init__()
 
     self.word_embeddings = nn.Embedding(vocab_size, embedding_length)
@@ -11,8 +37,18 @@ class NeuralNet(nn.Module):
 
 
   def forward(self, inputs):
+    """
+    Forward pass of NeuralNet
 
-    input = self.word_embeddings(inputs)  # convert text to embeddings
+    Args:
+      Inputs: list
+        Text
+
+    Returns:
+      output: torch.tensor
+        Outputs/Predictions
+    """
+    input = self.word_embeddings(inputs)  # Convert text to embeddings
     # Average the word embeddings in a sentence
     # Use torch.nn.functional.avg_pool2d to compute the averages
     pooled = F.avg_pool2d(input, (input.shape[1], 1)).squeeze(1)
@@ -30,7 +66,7 @@ class NeuralNet(nn.Module):
     return output
 
 
-# add event to airtable
+# Add event to airtable
 atform.add_event('Coding Exercise 3.1: Simple Feed Forward Net')
 
 # Uncomment to check your code
