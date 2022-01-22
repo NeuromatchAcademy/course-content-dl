b6cd97c1a8aec7b6b38f16f4f244fd5ea69805fa
GitHub Action <action@github.com>
2022-01-22 11:52:20 +0000

    Process tutorial notebooks

--- tutorials/W2D2_ModernConvnets/solutions/W2D2_Tutorial1_Solution_3a7a6a8e.py
+++ tutorials/W2D2_ModernConvnets/solutions/W2D2_Tutorial1_Solution_3a7a6a8e.py
@@ -3,4 +3,4 @@
   -- if number of paths=1 and channels per path=64 -> same architecture as ResNet, no parameter saving
   -- if number of paths=8 and channels per path=8 -> around half the number of parameters
   -- if number of paths=32 and channels per path=2 -> biggest parameter saving the more paths, the more saving
-"""
+""";
