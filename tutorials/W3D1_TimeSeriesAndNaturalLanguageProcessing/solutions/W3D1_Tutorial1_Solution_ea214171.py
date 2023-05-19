817341d6081bbef8319ca684f511fe45d8e9cc46
GitHub Action <action@github.com>
2023-05-19 23:20:49 +0000

    Process tutorial notebooks

--- tutorials/W3D1_TimeSeriesAndNaturalLanguageProcessing/solutions/W3D1_Tutorial1_Solution_ea214171.py
+++ tutorials/W3D1_TimeSeriesAndNaturalLanguageProcessing/solutions/W3D1_Tutorial1_Solution_ea214171.py
@@ -1,6 +1,6 @@
 class NeuralNet(nn.Module):
   """ A vanilla neural network. """
-  def __init__(self, batch_size, output_size, hidden_size, vocab_size,
+  def __init__(self, output_size, hidden_size, vocab_size,
                embedding_length, word_embeddings):
     """
     Constructs a vanilla Neural Network Instance.
@@ -26,18 +26,27 @@ class NeuralNet(nn.Module):
     """
     super(NeuralNet, self).__init__()
 
-    self.batch_size = batch_size
     self.output_size = output_size
     self.hidden_size = hidden_size
     self.vocab_size = vocab_size
     self.embedding_length = embedding_length
 
-    self.word_embeddings = nn.Embedding(vocab_size, embedding_length)
-    self.word_embeddings.weight = nn.Parameter(word_embeddings, requires_grad=False)
+    # self.word_embeddings = nn.EmbeddingBag(vocab_size, embedding_length, sparse=False)
+    self.word_embeddings = nn.EmbeddingBag.from_pretrained(embedding_fasttext.vectors)
+    self.word_embeddings.weight.requiresGrad = False
     self.fc1 = nn.Linear(embedding_length, hidden_size)
     self.fc2 = nn.Linear(hidden_size, output_size)
+    self.init_weights()
 
-  def forward(self, inputs):
+  def init_weights(self):
+      initrange = 0.5
+      # self.word_embeddings.weight.data.uniform_(-initrange, initrange)
+      self.fc1.weight.data.uniform_(-initrange, initrange)
+      self.fc1.bias.data.zero_()
+      self.fc2.weight.data.uniform_(-initrange, initrange)
+      self.fc2.bias.data.zero_()
+
+  def forward(self, inputs, offsets):
     """
     Compute the final labels by taking tokens as input.
 
@@ -49,18 +58,11 @@ class NeuralNet(nn.Module):
       out: Tensor
         Final prediction Tensor
     """
-    input = self.word_embeddings(inputs)  # convert text to embeddings
-    # Average the word embedddings in a sentence
-    # Use torch.nn.functional.avg_pool2d to compute the averages
-    pooled = F.avg_pool2d(input, (input.shape[1], 1)).squeeze(1)
+    embedded = self.word_embeddings(inputs, offsets)  # convert text to embeddings
     # Pass the embeddings through the neural net
     # Use ReLU as the non-linearity
-    x = self.fc1(pooled)
+    x = self.fc1(embedded)
     x = F.relu(x)
     x = self.fc2(x)
     output = F.log_softmax(x, dim=1)
     return output
-
-
-# add event to airtable
-atform.add_event('Coding Exercise 1: Neural Net for text classification')
