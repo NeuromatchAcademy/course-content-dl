3d8517d6ad38c5e2c70836c131a728043cb00b1f
GitHub Action <action@github.com>
2023-06-08 05:40:21 +0000

    Process tutorial notebooks

---
+++ tutorials/W3D1_TimeSeriesAndNaturalLanguageProcessing/solutions/W3D1_Tutorial2_Solution_0c69712b.py
@@ -0,0 +1,13 @@
+
+"""
+Here is one possible answer.
+1. The model is not well-trained or fine-tuned on the task of generating Python
+code from natural language instructions. It may have insufficient data,
+low quality data, or inappropriate hyperparameters.
+2. The model is not smart or reliable enough to write code for them.
+It may have bugs, glitches, or limitations that prevent it from working properly.
+3. I think the model-reported confidence for this output would be low, since
+the output has many errors and deviations from the instructions. However, the
+confidence may also depend on how the model is trained and calibrated, and how
+it estimates its own uncertainty and quality.
+""";
