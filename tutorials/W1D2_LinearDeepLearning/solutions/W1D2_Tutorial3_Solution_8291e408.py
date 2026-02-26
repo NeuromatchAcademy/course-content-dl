a9ccfe1a963efa8606e63db0f56f1e3b4f827705
Mahmoud Mohsen <99143276+Mahmoud12344@users.noreply.github.com>
2026-02-27 00:29:38 +0200

    Fix sigma calculation in weight initialization
    I have noticed that this ismplementation  of the function :
    
    \begin{equation}
    \mathcal{N}\left(\mu=0, ~~\sigma=\gamma \sqrt{\dfrac{1}{n_{in} + n_{out}}} \right)
    \end{equation}
     was mistaken to be devision in the code implementation
    So I edited it to have the correct multiplication operator between the gamma and the square root

--- tutorials/W1D2_LinearDeepLearning/solutions/W1D2_Tutorial3_Solution_8291e408.py
+++ tutorials/W1D2_LinearDeepLearning/solutions/W1D2_Tutorial3_Solution_8291e408.py
@@ -13,7 +13,7 @@ def ex_initializer_(model, gamma=1e-12):
   """
   for weight in model.parameters():
     n_out, n_in = weight.shape
-    sigma = gamma / math.sqrt(n_in + n_out)
+    sigma = gamma * math.sqrt(n_in + n_out)
     nn.init.normal_(weight, mean=0.0, std=sigma)
 
 
